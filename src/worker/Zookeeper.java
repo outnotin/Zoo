b74c1e8bf8441ade7ac5ee929b78316ef0785db9
outnotin <outnotin@gmail.com>
2016-07-13 15:00:23 +0700

    commit before rest

---
+++ src/worker/Zookeeper.java
@@ -0,0 +1,12 @@
+package worker;
+
+/**
+ * Created by Noppharat on 7/13/2016.
+ */
+public class Zookeeper extends Worker implements Working {
+    public String buildingZone;
+    @Override
+    public void doingDuty() {
+
+    }
+}
