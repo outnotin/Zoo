0b1e81e84c142780419e81ece7986c610c795dc8
outnotin <outnotin@gmail.com>
2016-07-15 09:38:18 +0700

    edit worker package

--- src/worker/Information.java
+++ src/worker/Information.java
@@ -4,9 +4,35 @@ package worker;
  * Created by Noppharat on 7/13/2016.
  */
 public class Information extends Worker implements Working {
+    private String buildingZone;
 
     @Override
-    public void doingDuty() {
+    public void doingDuty(String command) {
+        switch (command){
+            case "tellinfo" :
+                System.out.println("Information start telling info.");
+                tellingZooInfo();
+            case "announce":
+                System.out.println("Information start announcing.");
+                annoucing();
+                default:
+                    System.out.println("Information is not doing anythins.");
+        }
+    }
 
+    protected void tellingZooInfo(){
+
+    }
+
+    protected void annoucing(){
+
+    }
+
+    public String getBuildingZone() {
+        return buildingZone;
+    }
+
+    public void setBuildingZone(String buildingZone) {
+        this.buildingZone = buildingZone;
     }
 }
